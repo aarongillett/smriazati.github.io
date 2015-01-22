f9b02ab19965007402ca8635b9806f8ccae04be6
smriazati <sarahriazati@gmail.com>
2015-01-22 16:22:19 +0100

    mobile usability tests on story page

--- michael/js/story.js
+++ michael/js/story.js
@@ -3,29 +3,54 @@
 
 
 // grid resizer
-// still need to set max points here!!!
+
+// If the screen is smaller than the med-bp, 
+// the photo percent is .5 and there is no bottom padding
 
 
 function photoResizer() {
 
   var windowHeight = $(window).height();
-  var photoPercent = 0.8; // percent of height of screen
-  var imageHeight = photoPercent*windowHeight; // 
-  var imageWidth = (imageHeight*(.8)); 
+
+  if ($(window).width() > 1000) {
+
+    var photoPercent = 0.8; // percent of height of screen
+    var imageHeight = photoPercent*windowHeight; // 
+    var imageWidth = (imageHeight*(.8)); 
 
 
-  // add image width, image height to photo element
-  $(".photo img").css("height", imageHeight);
-  $(".photo img").css("width", imageWidth);
+    // add image width, image height to photo element
+    $(".photo img").css("height", imageHeight);
+    $(".photo img").css("width", imageWidth);
 
-  var noteWidth = imageWidth/1.5; // note is proportional to image 
-  $(".note img").css("width", noteWidth);
+    var noteWidth = imageWidth/1.5; // note is proportional to image 
+    $(".note img").css("width", noteWidth);
 
-  // now for some padding
-  var postPadding = ((1-photoPercent)/2)*windowHeight;
-  $(".post").css("padding-bottom", postPadding);
+    // now for some padding
+    var postPadding = ((1-photoPercent)/2)*windowHeight;
+    $(".post").css("padding-bottom", postPadding);
 
-  // horizontally center it
+  } else {
+
+    var photoPercent = 0.55; // percent of height of screen
+    var imageHeight = photoPercent*windowHeight; // 
+    var imageWidth = (imageHeight*(.8)); 
+
+
+    // add image width, image height to photo element
+    $(".photo img").css("height", imageHeight);
+    $(".photo img").css("width", imageWidth);
+
+    var noteWidth = imageWidth/(1.25); // note is proportional to image 
+    $(".note img").css("width", noteWidth);
+
+    // no padding
+    $(".post").css("padding-bottom", "0");
+
+  }
+
+
+
 
 
   // console.log("width " + imageWidth);
