622c05d1b577d78244da90f222d9595074bbff7b
smriazati <sarahriazati@gmail.com>
2015-01-26 17:01:25 +0100

    change close button, test history state

--- michael/js/index.js
+++ michael/js/index.js
@@ -1,3 +1,53 @@
+$(function() {
+  String.prototype.decodeHTML = function() {
+    return $("<div>", {html: "" + this}).html();
+  };
+
+  var $main = $("main"),
+  
+  init = function() {
+        $main.fadeIn(1000);
+        console.log("fade in!");
+  },
+  
+  ajaxLoad = function(html) {
+    document.title = html
+      .match(/<title>(.*?)<\/title>/)[1]
+      .trim()
+      .decodeHTML();
+
+    init();
+  },
+  
+  loadPage = function(href) {
+    $main.load(href + " main>*", ajaxLoad);
+  };
+  
+  init();
+  
+  $(window).on("popstate", function(e) {
+    if (e.originalEvent.state !== null) {
+      loadPage(location.href);
+    }
+  });
+
+  $(document).on("click", "a, area", function() {
+    var href = $(this).attr("href");
+
+    if (href.indexOf(document.domain) > -1
+      || href.indexOf(':') === -1)
+    {
+      history.pushState({}, '', href);
+      loadPage(href);
+      return false;
+    }
+  });
+});
+
+
+
+/// og 
+
 
 var currentSection, homeSection; 
 var mouseWheelTransition = false; 
