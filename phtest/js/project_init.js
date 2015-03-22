5ff1f636cf72699d82efd256e83a432c4c59e097
smriazati <sarahriazati@gmail.com>
2015-03-22 13:00:10 -0400

    testing js load probs

---
+++ phtest/js/project_init.js
@@ -0,0 +1,16 @@
+
+  $(document).ready(function() {
+
+    $('.project-slideshow').slick({
+      dots: true,
+      infinite: true,
+      speed: 100,
+      slidesToShow: 1,
+      arrows: false,
+      fade: true,
+      adaptiveHeight: true
+
+    });
+               
+  });
+
