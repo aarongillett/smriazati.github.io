057584261ceb1767e0aee649b47b55c2c137da67
smriazati <sarahriazati@gmail.com>
2015-03-22 13:02:19 -0400

    testing js load probs

--- phtest/js/projects.js
+++ phtest/js/projects.js
@@ -1,18 +1,8 @@
 
 function centerNav() {
-  // get width of image
   var imgWidth = $('.project-slideshow div img').width();
-  // console.log(imgWidth);
-
-  // get width of UL object
   var ulWidth = $('ul.slick-dots').width();
-  // console.log(ulWidth);
-
-  // calculate offset
   var ulOffset = (imgWidth / 2) - (ulWidth / 2);
-  // console.log(ulOffset);
-
-  // apply left margin
   $('ul.slick-dots').css('margin-left', ulOffset);  
 }
 
@@ -22,13 +12,8 @@ function centerNav() {
 /// Inits
 
 $(document).ready(function() {
-  $('.project-slideshow').on('init', function(event, slick){
-    centerNav();
-  });
-
-             
+  centerNav();
 });
-
 
 $( window ).resize(function() {
   centerNav();
