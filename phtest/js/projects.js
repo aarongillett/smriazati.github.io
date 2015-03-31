80420c49edaa743d38a609787e147cca40cad03e
smriazati <sarahriazati@gmail.com>
2015-03-31 12:52:11 -0400

    update pascal site

--- phtest/js/projects.js
+++ phtest/js/projects.js
@@ -11,22 +11,22 @@
 
 
 
-function centerNav() {
-  var imgWidth = $('.project-slideshow div img').width();
-  var ulWidth = $('ul.slick-dots').width();
-  var ulOffset = (imgWidth / 2) - (ulWidth / 2);
-  $('ul.slick-dots').css('margin-left', ulOffset);  
-}
+// function centerNav() {
+//   var imgWidth = $('.project-slideshow div img').width();
+//   var ulWidth = $('ul.slick-dots').width();
+//   var ulOffset = (imgWidth / 2) - (ulWidth / 2);
+//   $('ul.slick-dots').css('margin-left', ulOffset);  
+// }
 
 
 
 
-/// Inits
+// /// Inits
 
-$(document).ready(function() {
-  centerNav();
-});
+// $(document).ready(function() {
+//   centerNav();
+// });
 
-$( window ).resize(function() {
-  centerNav();
-});
+// $( window ).resize(function() {
+//   centerNav();
+// });
