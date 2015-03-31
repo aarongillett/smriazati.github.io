8b6b12cbc5d3ab1bd0e72a14d91b853c8f604c21
smriazati <sarahriazati@gmail.com>
2015-03-31 12:56:46 -0400

    add page loader to projects

--- phtest/js/project_init.js
+++ phtest/js/project_init.js
@@ -1,5 +1,11 @@
 
+
   $(document).ready(function() {
+
+// add in page loader 
+$(".project-wrapper").append('<div id="pageloader"><img src="../imgs/loading.gif" /> </div>')
+
+
 
     $('.project-slideshow').slick({
       dots: true,
